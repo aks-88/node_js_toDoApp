6c1887c4acdddb5bb160fe6eb68e45535ce63d91
Akshay Sudheer <akshaysudheer@CAP-LAP-759.local>
2021-08-13 15:58:04 +0530

    EJS List added

--- index.js
+++ index.js
@@ -4,8 +4,71 @@ const app=express();
 const request= require("request");
 const bodyParser= require("body-parser");
 
+var items= [];
+// const ejs= require("ejs");
+
+//using  ejs
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//rendering list
+
 app.get("/", function(req, res){
-    res.send("hello");
+    // res.sendFile(__dirname+"/index.html");
+    var today= new Date();
+    var options= {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+
+    }
+    var getDay= today.toLocaleDateString("en-US",options);
+    console.log(getDay)
+    // if (today.getDay() ===5||today.getDay() ===0){
+    //     res.send("Yay it's the weekend");
+    // } else{
+    //     res.write("<p>line1</p>");
+    //     res.write("<p>line1</p>");
+    //     res.send("bro! I have to work");
+    // }
+
+    /// ejs render list
+
+    const days_of_the_week= ['Sunday','Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    res.render('list',{kindOfDay:getDay, newListItems:items});
+});
+
+app.get("/stagging", function(req, res){
+    res.sendFile(__dirname+"/index.html");
+    var today= new Date();
+    console.log(today.getDay())
+    // if (today.getDay() ===5||today.getDay() ===0){
+    //     res.send("Yay it's the weekend");
+    // } else{
+    //     res.write("<p>line1</p>");
+    //     res.write("<p>line1</p>");
+    //     res.send("bro! I have to work");
+    // }
+    
+
+});
+
+app.get("/ping",function(req,res){
+    res.json("{'Name':'Swathy R'}");
+});
+
+app.post("/", function(req,res){
+    console.log(req.body);
+    
+    var item= req.body.newListItem;
+    console.log(item);
+    items.push(item);
+    console.log(items);
+
+    res.redirect("/")
+
+
 });
 
 app.listen(3000,function(){
